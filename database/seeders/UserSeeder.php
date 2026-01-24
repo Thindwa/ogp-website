b6cc1f0287c88b734a23a41337b6f0a93e3889b2
Thindwa <35875272+Thindwa@users.noreply.github.com>
2026-01-24 23:49:25 +0200

    Refactor UserSeeder to use updateOrCreate method

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,72 +11,54 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Get TWGs for assignment
         $twgs = TechnicalWorkingGroup::all();
 
-        // Create main admin user
-        User::create([
-            'name' => 'OGP Admin',
-            'email' => 'admin@ogp.mw',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
-            'email_verified_at' => now(),
-        ]);
+        // Main admin user
+        $admin = User::updateOrCreate(
+            ['email' => 'admin@ogp.mw'],
+            [
+                'name' => 'OGP Admin',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
 
-        // Create TWG users for each Technical Working Group
+        // TWG users
         $twgUsers = [
-            [
-                'name' => 'Anti-Corruption TWG Manager',
-                'email' => 'anticorruption@ogp.mw',
-                'password' => Hash::make('password'),
-                'role' => 'twg_manager',
-                'email_verified_at' => now(),
-            ],
-            [
-                'name' => 'Access to Information TWG Manager',
-                'email' => 'accesstoinfo@ogp.mw',
-                'password' => Hash::make('password'),
-                'role' => 'twg_manager',
-                'email_verified_at' => now(),
-            ],
-            [
-                'name' => 'Digital Governance TWG Manager',
-                'email' => 'digitalgovernance@ogp.mw',
-                'password' => Hash::make('password'),
-                'role' => 'twg_manager',
-                'email_verified_at' => now(),
-            ],
-            [
-                'name' => 'Natural Resources TWG Manager',
-                'email' => 'naturalresources@ogp.mw',
-                'password' => Hash::make('password'),
-                'role' => 'twg_manager',
-                'email_verified_at' => now(),
-            ],
-            [
-                'name' => 'Open Parliament TWG Manager',
-                'email' => 'openparliament@ogp.mw',
-                'password' => Hash::make('password'),
-                'role' => 'twg_manager',
-                'email_verified_at' => now(),
-            ],
+            ['name' => 'Anti-Corruption TWG Manager', 'email' => 'anticorruption@ogp.mw'],
+            ['name' => 'Access to Information TWG Manager', 'email' => 'accesstoinfo@ogp.mw'],
+            ['name' => 'Digital Governance TWG Manager', 'email' => 'digitalgovernance@ogp.mw'],
+            ['name' => 'Natural Resources TWG Manager', 'email' => 'naturalresources@ogp.mw'],
+            ['name' => 'Open Parliament TWG Manager', 'email' => 'openparliament@ogp.mw'],
         ];
 
-        foreach ($twgUsers as $index => $userData) {
-            $user = User::create($userData);
-            // Assign TWG if available
+        foreach ($twgUsers as $index => $data) {
+            $user = User::updateOrCreate(
+                ['email' => $data['email']],
+                [
+                    'name' => $data['name'],
+                    'password' => Hash::make('password'),
+                    'role' => 'twg_manager',
+                    'email_verified_at' => now(),
+                ]
+            );
+
+            // Assign TWG
             if ($twgs->count() > $index) {
                 $user->update(['technical_working_group_id' => $twgs[$index]->id]);
             }
         }
 
-        // Create a general content manager
-        User::create([
-            'name' => 'Content Manager',
-            'email' => 'content@ogp.mw',
-            'password' => Hash::make('password'),
-            'role' => 'content_manager',
-            'email_verified_at' => now(),
-        ]);
+        // General content manager
+        User::updateOrCreate(
+            ['email' => 'content@ogp.mw'],
+            [
+                'name' => 'Content Manager',
+                'password' => Hash::make('password'),
+                'role' => 'content_manager',
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
